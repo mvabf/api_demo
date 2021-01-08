89a5e5a15e91df33a69687f4d1c4c39380a28013
mvabf <mvabf_dev@outlook.com>
2021-01-08 19:58:14 -0300

    projeto criado, rota list ok

---
+++ database/migrations/2021_01_08_222922_create_products.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 30);
+            $table->double('price');
+            $table->integer('quantity');
+            $table->string('department', 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(['prefix' => 'api'], function () {
+    
+    Route::get('/', function () {
+    return response()->json(['message'=>'Products API', 'status'=> 'Connected']);
+});
+    Route::resource('products', 'ProductsController');
+
+});
+
+Route::get('/', function () {
+    return redirect('api');
+});
+
